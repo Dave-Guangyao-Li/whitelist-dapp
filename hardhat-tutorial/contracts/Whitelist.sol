ecc6b2988d4f5396539372ef0d772adb4efc9260
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2022-08-30 11:00:28 -0700

    writing Whitelist smart contract

--- hardhat-tutorial/contracts/Whitelist.sol
+++ hardhat-tutorial/contracts/Whitelist.sol
@@ -3,9 +3,30 @@ pragma solidity ^0.8.0;
 
 contract Whitelist {
     //maximum number of addresses that can be whitelisted
-    uint8 public maxWhitelistAddress;
+    uint8 public maxWhitelistedAddresses;
 
-    constructor(uint8 _maxWhitelistAddress) {
-        maxWhitelistAddress = _maxWhitelistAddress;
+    //keep track of number of addresses that are whitelisted
+    uint8 public numAddressesWhitelisted;
+
+    //hashmap of whitelisted addresses, faster than array, give value directly avoid loop, less gas spent
+    mapping(address => bool) public whitelistedAddresses;
+
+    constructor(uint8 _maxWhitelistedAddresses) {
+        maxWhitelistedAddresses = _maxWhitelistedAddresses;
+    }
+
+    //add address to whitelist
+    function addressToWhitelist() public {
+        //msg.sender is the address of user who called this function
+        require(
+            !whitelistedAddresses[msg.sender], //Calling require(x) where x evaluates to false.
+            "Sender already in the whitelist"
+        );
+        require(
+            numAddressesWhitelisted < maxWhitelistedAddresses,
+            "Max limit reached"
+        );
+        whitelistedAddresses[msg.sender] = true;
+        numAddressesWhitelisted += 1; //increase number of address whitelisted
     }
 }
